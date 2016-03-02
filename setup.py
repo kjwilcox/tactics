5ff15542025a598e8338a31334d876a7a9849731
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-02 12:33:25 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='tactics',
+  version='0.1',
+  description='Tactics RPG',
+  author='Kyle Wilcox',
+  author_email='k.j.wilcox@gmail.com',
+  py_modules=['tactics'],
+)
