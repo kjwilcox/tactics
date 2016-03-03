d2714abcdafa78858c4b1ad5c50df1f1ea5d3b68
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-02 20:57:59 -0800

    Furhter mock out pyglet calls to allow testing cache logic

--- tactics/tests/test_window.py
+++ tactics/tests/test_window.py
@@ -10,3 +10,12 @@ def test_get_window_uses_cached_window():
     actual_window = module.get_window()
 
     assert mock_window == actual_window
+
+
+def test_get_window_creates_window():
+    mock_window = flexmock()
+    flexmock(module).should_receive("create_window").and_return(mock_window)
+
+    actual_window = module.get_window()
+
+    assert mock_window == actual_window
--- tactics/window.py
+++ tactics/window.py
@@ -4,11 +4,18 @@ import pyglet
 _window = None
 
 
+def create_window():
+    """
+    :return: pyglet window
+    """
+    return pyglet.window.Window()
+
+
 def get_window():
     """
     :return: Main pyglet game window.
     """
     global _window
     if not _window:
-        _window = pyglet.window.Window()
+        _window = create_window()
     return _window
