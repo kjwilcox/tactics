c0e2307ad1a9ad62a0fb84a16b53c6471ea0ec1c
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-02 17:10:35 -0800

    Added trove classifiers and fixed indentation

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@
 from distutils.core import setup
 
 setup(
-  name='tactics',
-  version='0.1',
-  description='Tactics RPG',
-  author='Kyle Wilcox',
-  author_email='k.j.wilcox@gmail.com',
-  py_modules=['tactics'],
+    name='tactics',
+    version='0.1',
+    description='Tactics RPG',
+    author='Kyle Wilcox',
+    author_email='k.j.wilcox@gmail.com',
+    py_modules=['tactics'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+    ],
 )
