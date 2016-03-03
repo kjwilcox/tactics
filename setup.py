a244bd93f3e3c335221ddcad738176670f55c44b
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-02 20:22:23 -0800

    Backout crappy test

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
         'Programming Language :: Python :: 3',
     ],
     install_requires=[
-        "pyglet",
+        'pyglet',
     ],
 )
--- tactics/tests/test_main.py
+++ tactics/tests/test_main.py
@@ -1,22 +1,5 @@
-import flexmock
-
 from tactics import main as module
 
 
 def test_update_does_not_raise():
     module.update(1)
-
-
-def test_main_does_not_raise():
-    flexmock(
-        module,
-        pyglet=flexmock(window=flexmock(Window=flexmock(
-            event=lambda: None,
-        )))
-    )
-
-    flexmock(module).should_receive('pyglet.clock.ClockDisplay')
-    flexmock(module).should_receive('pyglet.clock.schedule_interval')
-    flexmock(module).should_receive('pyglet.app.run')
-
-    module.main()
