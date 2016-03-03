e01e25b3846f03ba29856b222bb0c5ece684936b
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-02 20:59:51 -0800

    Fix broken test

--- tactics/tests/test_window.py
+++ tactics/tests/test_window.py
@@ -14,6 +14,7 @@ def test_get_window_uses_cached_window():
 
 def test_get_window_creates_window():
     mock_window = flexmock()
+    flexmock(module).should_receive("_window").and_return(None)
     flexmock(module).should_receive("create_window").and_return(mock_window)
 
     actual_window = module.get_window()
