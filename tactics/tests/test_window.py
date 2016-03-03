0b34a2994c4ec0d9911c024fb9e30de699b7d557
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-02 20:50:48 -0800

    Fix another test typo

--- tactics/tests/test_window.py
+++ tactics/tests/test_window.py
@@ -5,7 +5,7 @@ from flexmock import flexmock
 
 def test_get_window_uses_cached_window():
     mock_window = flexmock()
-    flexmock(module).should_receive("window").and_return(mock_window)
+    flexmock(module).should_receive("_window").and_return(mock_window)
 
     actual_window = module.get_window()
 
