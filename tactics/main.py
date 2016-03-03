219b2575e1288deeccd898d85e16e41378724549
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-02 19:16:38 -0800

    Barebones game loop

--- tactics/main.py
+++ tactics/main.py
@@ -1,8 +1,31 @@
 import pyglet
 
+TARGET_FRAMERATE = 60
+TICK_RATE = 1 / TARGET_FRAMERATE  # Frame time, in seconds
+
+
+def update(dt):
+    """
+    Performs the game state simulation.
+    :param dt: Time in seconds since last update.
+    """
+    pass
+
 
 def main():
-    pyglet.window.Window()
+    window = pyglet.window.Window()
+
+    fps_display = pyglet.clock.ClockDisplay()
+
+    @window.event
+    def on_draw():
+        """ Draw screen. """
+        window.clear()
+        fps_display.draw()
+
+    # Register game state update logic
+    pyglet.clock.schedule_interval(update, TICK_RATE)
+
     pyglet.app.run()
 
 
---
+++ tactics/tests/test_main.py
@@ -0,0 +1,5 @@
+from tactics import main as module
+
+
+def test_update_does_not_raise():
+    module.update(1)
