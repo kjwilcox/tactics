7e8cc33b72a53d41196e080bcdf9df2d00044509
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-06 18:10:00 -0800

    Disable shadow window

--- tactics/tests/test_tilemap.py
+++ tactics/tests/test_tilemap.py
@@ -2,4 +2,6 @@ from tactics import tilemap as module
 
 
 def test_tilemap_loads_resources_and_does_not_raise():
+    import pyglet
+    pyglet.options['shadow_window'] = False
     module.TileMap(16, 16, 16)
