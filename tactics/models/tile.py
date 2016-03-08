6434dcf17033ca5fefa52e2ef6c3f12641dc24ff
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-07 22:34:36 -0800

    Don't read file at import time

--- tactics/models/tile.py
+++ tactics/models/tile.py
@@ -2,19 +2,22 @@ from tactics import loader
 
 
 class Tile:
+    def __init__(self):
+        self.image = loader.load_image(self.image_path)
+
     def draw(self):
         pass
 
 
 class GrassTile(Tile):
     name = "Grass"
-    image = loader.load_image("grass.png")
+    image_path = "grass.png"
     cost = 1
     defense = 0
 
 
 class WaterTile(Tile):
     name = "Water"
-    image = loader.load_image("water.png")
+    image_path = "water.png"
     cost = 3
     defense = 0
