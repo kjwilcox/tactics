4e3e9a8d1e32576c62bd14d9c555de3665466fd9
Kyle Wilcox <k.j.wilcox@gmail.com>
2016-03-07 22:38:22 -0800

    Mock out image loading in tilemap test

--- tactics/tests/models/test_tile.py
+++ tactics/tests/models/test_tile.py
@@ -3,7 +3,7 @@ from flexmock import flexmock
 from tactics.models import tile as module
 
 
-def mock_loader():
+def mock_image_loader():
     flexmock(module).should_receive(
         'loader.load_image'
     ).and_return(
@@ -12,10 +12,10 @@ def mock_loader():
 
 
 def test_grass_tile_does_not_raise():
-    mock_loader()
+    mock_image_loader()
     module.GrassTile()
 
 
 def test_water_tile_does_not_raise():
-    mock_loader()
+    mock_image_loader()
     module.WaterTile()
--- tactics/tests/models/test_tilemap.py
+++ tactics/tests/models/test_tilemap.py
@@ -3,7 +3,16 @@ from tactics.models import tilemap as module
 from flexmock import flexmock
 
 
+def mock_image_loader():
+    flexmock(module).should_receive(
+        'loader.load_image'
+    ).and_return(
+        flexmock()
+    )
+
+
 def test_tilemap_creation_and_draw_does_not_raise():
+    mock_image_loader()
     flexmock(module).should_receive(
         'multimedia.SpriteBatch'
     ).and_return(flexmock(draw=lambda: None))
